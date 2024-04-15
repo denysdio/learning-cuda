3e012c5f9e3fce20f08625017fd996d1bf413003
diohsilver@gmail.com <diohsilver@gmail.com>
2024-04-15 18:56:06 -0300

    Include kernel to perform BFS

--- traversal.cu
+++ traversal.cu
@@ -1,3 +1,4 @@
+#include <climits>
 #include <stdio.h>
 #define N_VERTICES 9
 #define N_EDGES 15
@@ -73,17 +74,73 @@ __global__ void foo(graph_t *g, int n) {
   }
 }
 
+__global__ void bfs_kernel(graph_t *graph, int *level, int *new_vertex_visited,
+                           int *curr_level) {
+  int vertex = blockDim.x * blockIdx.x + threadIdx.x;
+
+  if (vertex < graph->numVertices) {
+    printf("graph n_vertices = %d\n", graph->numVertices);
+
+    if (level[vertex] == *curr_level - 1) {
+      for (int edge = graph->srcPtrs[vertex]; edge < graph->srcPtrs[vertex + 1];
+           ++edge) {
+        printf("edge id = %d\n", edge);
+        int neighbor = graph->dst[edge];
+        printf("neighbor id = %d\n", neighbor);
+        if (level[neighbor] == INT_MAX) { // neighbor not visited
+
+          printf("new neighbor id added to level %d = %d\n", *curr_level,
+                 neighbor);
+          level[neighbor] = *curr_level;
+          *new_vertex_visited = 1;
+        }
+      }
+    }
+  }
+}
+
 int main(void) {
   graph_t *g = init_graph();
+  int *level, *new_visited, *curr_level;
+
   graph_t *g_dev;
+  int *level_dev, *new_visited_dev, *curr_level_dev;
+
+  // Init BFS variables
+  level = (int *)malloc(N_VERTICES * sizeof(int));
+  curr_level = (int *)malloc(sizeof(int));
+  new_visited = (int *)malloc(sizeof(int));
+
+  level[0] = 0;
+  for (int i = 1; i < N_VERTICES; ++i) {
+    level[i] = INT_MAX;
+  }
+  *curr_level = 1;
+  *new_visited = 0;
 
   cudaMalloc((void **)&g_dev, sizeof(graph_t));
-  cudaMemcpy(g_dev, g, sizeof(graph_t), cudaMemcpyHostToDevice);
+  cudaMalloc((void **)&level_dev, N_VERTICES * sizeof(int));
+  cudaMalloc((void **)&curr_level_dev, sizeof(int));
+  cudaMalloc((void **)&new_visited_dev, sizeof(int));
 
-  foo<<<N_VERTICES / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(g_dev, 1);
+  cudaMemcpy(g_dev, g, sizeof(graph_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(level_dev, level, N_VERTICES * sizeof(int),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(curr_level_dev, curr_level, sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(new_visited_dev, new_visited, sizeof(int), cudaMemcpyHostToDevice);
+
+  bfs_kernel<<<N_VERTICES / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(
+      g_dev, level_dev, new_visited, curr_level);
 
   free(g);
+  free(level);
+  free(new_visited);
+  free(curr_level);
+
   cudaFree(g_dev);
+  cudaFree(level_dev);
+  cudaFree(new_visited_dev);
+  cudaFree(curr_level_dev);
 
   // print_graph(g);
   return 0;
